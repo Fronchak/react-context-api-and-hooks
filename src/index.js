21cbfd170a1b0ce7acf3cb17d844d1c2af88055c
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-01-12 19:47:11 -0300

    Concluded CounterContext

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 import './styles/global-styles.css';
 
@@ -8,6 +9,8 @@ import {Home} from './templates/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,5 +1,7 @@
+import { useCounterContext } from "../../contexts/CounterContext"
 
 export const Home = () => {
+  const [state, dispatch] = useCounterContext();
 
   return (
     <div>
